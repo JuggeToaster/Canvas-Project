4e58bfb2e181b9af5774605f566ddb67a056fe14
JuggeToaster <matijadjurdjek11@gmail.com>
2022-04-19 09:42:26 +0200

    Videon är på 38.25

--- script.js
+++ script.js
@@ -5,57 +5,83 @@ myCanvas.width = window.innerWidth;
 myCanvas.height = window.innerHeight;
 let c = myCanvas.getContext("2d");
 
-//Position
-x = 100;
-y = 100;
-radius = 60;
-
-//Cirkel
-c.beginPath();
-c.arc(x, y, radius, 0, 2 * Math.PI);
-c.stroke();
-
-//Användarinput
-document.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "ArrowUp":
-      console.log("Nu trycker användaren på uppåtpilen");
-      y -= 10;
-      break;
-    case "ArrowDown":
-      console.log("Nu trycker användaren på nedåtpilen");
-      y += 10;
-      break;
-    case "ArrowLeft":
-      console.log("Nu trycker användaren på vänsterpilen");
-      x -= 10;
-      break;
-    case "ArrowRight":
-      x += 10;
-      console.log("Nu trycker användaren på högerpilen");
-      break;
-    default:
-      break;
+class Boundary {
+  static width = 40;
+  static height = 40;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
   }
-});
-document.addEventListener("click", (e) => {
-  if (e.x > x - radius && e.x < x + radius) {
-    if (e.y > y - radius && e.y < y + radius) {
-      console.log("Nu klickar användaren innanför cirkeln");
-    }
-  } else {
-    console.log("Användaren klickar utanför cirkeln");
+  draw() {
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
-});
-
-// Animeringsfunktion
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight); // Denna rad rensar skärmen
-
-  c.beginPath();
-  c.arc(x, y, radius, 0, 2 * Math.PI);
-  c.stroke();
 }
 
-animate();
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 15;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "yellow";
+    c.fill();
+    c.closePath();
+  }
+}
+
+const map = [
+  ["-", "-", "-", "-", "-", "-"],
+  ["-", " ", " ", " ", " ", "-"],
+  ["-", " ", "-", "-", " ", "-"],
+  ["-", " ", " ", " ", " ", "-"],
+  ["-", "-", "-", "-", "-", "-"],
+];
+
+const boundaries = [];
+const player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch (symbol) {
+      case "-":
+        boundaries.push(
+          new Boundary({
+            position: {
+              x: Boundary.width * j,
+              y: Boundary.height * i,
+            },
+          })
+        );
+        break;
+    }
+  });
+});
+
+boundaries.forEach((boundary) => {
+  boundary.draw();
+});
+
+player.draw();
+
+window.addEventListener("keydown", ({ key }) => {
+  console.log(key);
+  switch (key) {
+    case "w":
+      player.velocity.y = -5;
+      break;
+  }
+});
